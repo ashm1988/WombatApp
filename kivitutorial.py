7b7679bef51ea5d31ac2ad335cb8810aff1fe356
amcfarlane <amcfarlane@tradevela.com>
2019-05-22 17:16:49 +1000

    learning kivy

--- kivitutorial.py
+++ kivitutorial.py
@@ -1,24 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.scatter import Scatter
-from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 
 
-class playingApp(App):
+class MyApp(App):
     def build(self):
-        f = FloatLayout()
-        s = Scatter()
-        l = Label(text='this label')
-
-        f.add_widget(l)
-        s.add_widget(l)
-        return f
+        return FloatLayout()
 
 
-def main():
-    playingApp().run()
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    MyApp().run()
